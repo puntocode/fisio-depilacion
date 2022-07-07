48b44035b30dd9f77c0de0b9cec74e0827f6549c
Federico Lopez <federico.lopez@mtel.local>
2022-07-07 14:13:05 -0400

    develop ecosystem config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Depilación',
+    title: 'Fisiostetik - Depilación',
     htmlAttrs: {
       lang: 'es'
     },
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
     ],
   },
 
